29aca44b467f60ae4efdc02751c117bc9bdddf31
masagin <naganobol6212@gmail.com>
2026-05-02 23:30:30 +0900

    Initial setup: Rails 8 + Tailwind + PostgreSQL + Vite + React + TypeScript

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    RubyPlugin(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './app/javascript'),
+    },
+  },
+})
