25067f213494800fda3143c6095c3d5f607c4ef9
masagin <naganobol6212@gmail.com>
2026-05-02 23:39:32 +0900

    Fix path to match Vite Ruby's app/frontend convention

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './app/javascript'),
+      '@': path.resolve(__dirname, './app/frontend'),
     },
   },
 })
