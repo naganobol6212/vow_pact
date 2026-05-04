552c70259e3636d526406503ee96d9869d90bfe0
masagin <naganobol6212@gmail.com>
2026-05-04 18:03:36 +0900

    ApplicationController に CSRF 保護を追加
    Cookie ベースのセッション認証では SameSite=Lax だけでは不十分なため、
    ActionController::RequestForgeryProtection を include し
    protect_from_forgery with: :exception で明示的に有効化。
    
    API mode（ActionController::API）はデフォルトで CSRF 検証 OFF のため、
    Cookie 認証を使う以上は明示が必須。
    
    Refs #17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
   include Authentication
+
+  protect_from_forgery with: :exception
 end
